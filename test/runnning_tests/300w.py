925f1ff943f609368d413d94ef60aedff4f4c508
musyoku <musyoku@users.noreply.github.com>
2017-10-22 18:43:57 +0900

    :v:

---
+++ test/runnning_tests/300w.py
@@ -0,0 +1,188 @@
+import argparse, os, sys
+import cv2
+
+class BoundingBox:
+	def __init__(self):
+		self.left = 0
+		self.top = 0
+		self.right = 0
+		self.bottom = 0
+
+	def move_y(self, move):
+		self.top += move
+		self.bottom += move
+
+	def move_x(self, move):
+		self.top += move
+		self.bottom += move
+
+	def cast(self):
+		self.left = int(self.left)
+		self.top = int(self.top)
+		self.right = int(self.right)
+		self.bottom = int(self.bottom)
+
+	def width(self):
+		return self.right - self.left
+
+	def height(self):
+		return self.bottom - self.top
+
+def load_images(base_directory, category):
+	directory = os.path.join(base_directory, category)
+	images = []
+	fs = os.listdir(directory)
+	for filename in fs:
+		print("loading", filename)
+		if filename.endswith(".png"):
+			image_rgb = cv2.imread(os.path.join(directory, filename))
+			image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
+			images.append((image_rgb, image_gray, filename.replace(".png", "")))
+	return images
+
+def load_annotations(base_directory, category):
+	directory = os.path.join(base_directory, category)
+	annotations = {}
+	fs = os.listdir(directory)
+	for filename in fs:
+		print("loading", filename)
+		if filename.endswith(".pts"):
+			with open(os.path.join(directory, filename), "r") as f:
+				annotation = f.read().strip().split("\n")
+				assert len(annotation) == 72
+				landmarks = []
+				for location_str in annotation[3:-1]:
+					location = location_str.split(" ")
+					landmarks.append((float(location[0]), float(location[1])))
+				annotations[filename.replace(".pts", "")] = landmarks
+	return annotations
+
+# expand the bounding box
+def expand_bounding_box(bbox, image_height, image_width, padding):
+	padding = min(padding, bbox.left)
+	padding = min(padding, bbox.top)
+	padding = min(padding, image_width - bbox.right)
+	padding = min(padding, image_height - bbox.bottom)
+
+	bbox.left = int(max(0, bbox.left - padding))
+	bbox.top = int(max(0, bbox.top - padding))
+	bbox.right = int(min(image_width, bbox.right + padding))
+	bbox.bottom = int(min(image_height, bbox.bottom + padding))
+
+def get_bounding_box(landmarks, image_height, image_width):
+	bbox = BoundingBox()
+	bbox.left = landmarks[0][0]
+	bbox.top = landmarks[0][1]
+	bbox.right = landmarks[0][0]
+	bbox.bottom = landmarks[0][1]
+
+	for (x, y) in landmarks:
+		if x > bbox.right:
+			bbox.right = x
+		if x < bbox.left:
+			bbox.left = x
+		if y > bbox.bottom:
+			bbox.bottom = y
+		if y < bbox.top:
+			bbox.top = y
+
+	bbox.cast()
+
+	# make the bounding box square
+	bbox_width = bbox.width()
+	bbox_height = bbox.height()
+
+	if bbox_width > bbox_height:
+		diff = (bbox_width - bbox_height) // 2
+		mod = (bbox_width - bbox_height) % 2
+		bbox.top = bbox.top - diff
+		bbox.bottom = bbox.bottom + diff + mod
+		if bbox.top < 0:
+			bbox.move_y(-bbox.top)
+			assert bbox.bottom <= image_height
+		elif bbox.bottom > image_height:
+			bbox.move_y(image_height - bbox.bottom)
+			assert bbox.top >= 0
+
+	elif bbox_width < bbox_height:
+		diff = (bbox_height - bbox_width) // 2
+		mod = (bbox_height - bbox_width) % 2
+		bbox.left = bbox.left - diff
+		bbox.right = bbox.right + diff + mod
+		if bbox.left < 0:
+			bbox.move_x(-bbox.left)
+			assert bbox.right <= image_width
+		elif bbox.right > image_width:
+			bbox.move_x(image_width - bbox.right)
+			assert bbox.left >= 0
+
+	bbox_width = bbox.width()
+	bbox_height = bbox.height()
+	assert bbox_width == bbox_height
+
+	padding = bbox_width * 0.3
+
+	expand_bounding_box(bbox, image_height, image_width, padding)
+
+	return bbox
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--dataset-directory", "-dataset", type=str, default=None)
+	parser.add_argument("--output-directory", "-out", type=str, default=None)
+	args = parser.parse_args()
+
+	assert args.dataset_directory is not None
+	assert args.output_directory is not None
+
+	try:
+		os.mkdir(args.output_directory)
+	except:
+		pass
+
+	annotations = {}
+	annotations = dict(annotations, **load_annotations(args.dataset_directory, "01_Indoor"))
+	annotations = dict(annotations, **load_annotations(args.dataset_directory, "02_Outdoor"))
+
+	images = []
+	images += load_images(args.dataset_directory, "01_Indoor")
+	images += load_images(args.dataset_directory, "02_Outdoor")
+
+	for (image_rgb, image_gray, filename) in images:
+		print("processing", filename)
+		assert filename in annotations
+		landmarks = annotations[filename]
+		white = (255, 255, 255)
+
+		image_height = image_rgb.shape[0]
+		image_width = image_rgb.shape[1]
+
+		landmark_center = [0, 0]
+		for (x, y) in landmarks:
+			landmark_center[0] += x
+			landmark_center[1] += y
+
+		# import pdb;pdb.set_trace()
+
+		try:
+			bbox = get_bounding_box(landmarks, image_height, image_width)
+		except Exception as e:
+			import traceback
+			traceback.print_exc()
+			continue
+
+		landmark_center[0] = int(landmark_center[0] / len(landmarks))
+		landmark_center[1] = int(landmark_center[1] / len(landmarks))
+		
+		for (x, y) in landmarks:
+			x = int(x)
+			y = int(y)
+			cv2.line(image_rgb, (x - 4, y), (x + 4, y), white, 1)
+			cv2.line(image_rgb, (x, y - 4), (x, y + 4), white, 1)
+
+		cv2.rectangle(image_rgb, (bbox.left, bbox.top), (bbox.right, bbox.bottom), white, thickness=2)
+		cv2.imwrite(os.path.join(args.output_directory, "%s.jpg" % filename), image_rgb)
+
+if __name__ == "__main__":
+	main()
--- test/runnning_tests/helen.py
+++ test/runnning_tests/helen.py
@@ -10,16 +10,33 @@ def load_images(base_directory, number):
 		print("loading", filename)
 		image_rgb = cv2.imread(os.path.join(directory, filename))
 		image_gray = cv2.cvtColor(image_rgb, cv2.COLOR_BGR2GRAY)
-		images.append((image_gray, filename))
+		images.append((image_rgb, image_gray, filename.replace(".jpg", "")))
 	return images
+
+def load_annotations(base_directory):
+	annotations = {}
+	directory = os.path.join(base_directory, "annotation")
+	fs = os.listdir(directory)
+	for filename in fs:
+		print("loading", filename)
+		with open(os.path.join(directory, filename), "r") as f:
+			annotation = f.read().strip().split("\n")
+			assert len(annotation) == 195
+			image_name = annotation[0]
+			landmarks = []
+			for location_str in annotation[1:]:
+				location = location_str.split(" , ")
+				landmarks.append((float(location[0]), float(location[1])))
+			annotations[image_name] = landmarks
+	return annotations
 
 def main():
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--helen-directory", "-helen", type=str, default=None)
+	parser.add_argument("--dataset-directory", "-dataset", type=str, default=None)
 	parser.add_argument("--output-directory", "-out", type=str, default=None)
 	args = parser.parse_args()
 
-	assert args.helen_directory is not None
+	assert args.dataset_directory is not None
 	assert args.output_directory is not None
 
 	try:
@@ -27,37 +44,42 @@ def main():
 	except:
 		pass
 
-	cv_cascade_path = "../../cv_haarcascade.xml"
-	cv_cascade = cv2.CascadeClassifier(cv_cascade_path)
-
 	dlib_detector = dlib.get_frontal_face_detector()
-
+	annotations = load_annotations(args.dataset_directory)
 	images = []
-	images += load_images(args.helen_directory, 1)
-	images += load_images(args.helen_directory, 2)
-	images += load_images(args.helen_directory, 3)
-	images += load_images(args.helen_directory, 4)
-	images += load_images(args.helen_directory, 5)
+	images += load_images(args.dataset_directory, 1)
+	images += load_images(args.dataset_directory, 2)
+	images += load_images(args.dataset_directory, 3)
+	images += load_images(args.dataset_directory, 4)
+	images += load_images(args.dataset_directory, 5)
 
-	for (image, filename) in images:
+	for (image_rgb, image_gray, filename) in images:
 		print("processing", filename)
+		assert filename in annotations
+		landmarks = annotations[filename]
+		white = (255, 255, 255)
 
-		# OpenCV
-		_image = image.copy()
-		face_rects = cv_cascade.detectMultiScale(_image, scaleFactor=1.71, minNeighbors=2, minSize=(30, 30))
-		color = (255, 255, 255)
+		image_width = image_rgb.shape[1]
+		landmark_center = [0, 0]
+		for (x, y) in landmarks:
+			landmark_center[0] += x
+			landmark_center[1] += y
+		landmark_center[0] = int(landmark_center[0] / len(landmarks))
+		landmark_center[1] = int(landmark_center[1] / len(landmarks))
+		
+		for (x, y) in landmarks:
+			x = int(x)
+			y = int(y)
+			cv2.line(image_rgb, (x - 4, y), (x + 4, y), white, 1)
+			cv2.line(image_rgb, (x, y - 4), (x, y + 4), white, 1)
+
+		face_rects = dlib_detector(image_gray, 0)
 		if len(face_rects) > 0:
 			for rect in face_rects:
-				cv2.rectangle(_image, tuple(rect[0:2]),tuple(rect[0:2] + rect[2:4]), color, thickness=2)
-			cv2.imwrite(os.path.join(args.output_directory, "%s.cv.jpg" % filename), _image)
+				if rect.contains(landmark_center[0], landmark_center[1]):
+					cv2.rectangle(image_rgb, (rect.left(), rect.top()), (rect.right(), rect.bottom()), white, thickness=2)
 
-		# Dlib
-		_image = image.copy()
-		face_rects = dlib_detector(_image, 0)
-		if len(face_rects) > 0:
-			for rect in face_rects:
-				cv2.rectangle(_image, (rect.left(), rect.top()), (rect.right(), rect.bottom()), color, thickness=2)
-			cv2.imwrite(os.path.join(args.output_directory, "%s.dlib.jpg" % filename), _image)
+		cv2.imwrite(os.path.join(args.output_directory, "%s.jpg" % filename), image_rgb)
 
 if __name__ == "__main__":
 	main()
